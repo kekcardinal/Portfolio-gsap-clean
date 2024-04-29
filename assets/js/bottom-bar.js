e77234b58b82872751d0d9bec12d8d62d46aa288
kekcardinal <103967680+kekcardinal@users.noreply.github.com>
2024-04-29 17:31:07 -0400

    tessst

--- assets/js/bottom-bar.js
+++ assets/js/bottom-bar.js
@@ -1,13 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
   // Check if the user is on a mobile device
-  if (/Mobi|Android/i.test(navigator.userAgent)) {
-    // Calculate the height of the bottom browser bar
-    const bottomBarHeight = window.outerHeight - window.innerHeight;
 
-    // Update the content of the hero_texte div with the bottom bar height
-    const heroTexteDiv = document.querySelector(".test");
-    if (heroTexteDiv) {
-      heroTexteDiv.innerHTML = `<p>Bottom Bar Height: ${bottomBarHeight}px</p>`;
-    }
+  // Calculate the height of the bottom browser bar
+  const bottomBarHeight = window.outerHeight - window.innerHeight;
+
+  // Update the content of the hero_texte div with the bottom bar height
+  const heroTexteDiv = document.querySelector(".test");
+  if (heroTexteDiv) {
+    heroTexteDiv.innerHTML = `<p>Bottom Bar Height: ${bottomBarHeight}px</p>`;
   }
 });
