f3245473d337d37aea7136c52f997dbbd0ae4a84
kekcardinal <103967680+kekcardinal@users.noreply.github.com>
2024-04-29 17:41:47 -0400

    test again

--- assets/js/bottom-bar.js
+++ assets/js/bottom-bar.js
@@ -1,25 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
-  let previousHeight = window.innerHeight; // Initial window height
-
   // Check if the user is on a mobile device
-  if (/Mobi|Android/i.test(navigator.userAgent)) {
-    // Function to update bottom bar height
-    function updateBottomBarHeight() {
-      const currentHeight = window.innerHeight;
-      const bottomBarHeight = previousHeight - currentHeight;
-      previousHeight = currentHeight;
 
-      // Update the content of the hero_texte div with the bottom bar height
-      const heroTexteDiv = document.querySelector(".test");
-      if (heroTexteDiv) {
-        heroTexteDiv.innerHTML = `<p>Bottom Bar Height: ${bottomBarHeight}px</p>`;
-      }
-    }
+  // Calculate the height of the bottom browser bar
+  const bottomBarHeight = window.outerHeight - window.innerHeight;
 
-    // Initial update
-    updateBottomBarHeight();
-
-    // Listen for window resize events to update bottom bar height
-    window.addEventListener("resize", updateBottomBarHeight);
+  // Update the content of the hero_texte div with the bottom bar height
+  const heroTexteDiv = document.querySelector(".test");
+  if (heroTexteDiv) {
+    heroTexteDiv.innerHTML = `<p>Bottom Bar Height: ${bottomBarHeight}px</p>`;
   }
 });
