8bb6fd473f903bce3254d8ca9aaac2cb1916c160
kekcardinal <103967680+kekcardinal@users.noreply.github.com>
2024-04-29 18:07:12 -0400

    tessst

--- assets/js/bottom-bar.js
+++ assets/js/bottom-bar.js
@@ -1,24 +1,41 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const height =
-    window.innerHeight ||
-    document.documentElement.clientHeight ||
-    document.body.clientHeight;
+  // Calculate top and bottom bar heights
+  const totalScreenHeight = screen.availHeight;
+  const visibleWindowHeight = window.innerHeight;
+  const topBarHeight = totalScreenHeight - visibleWindowHeight;
+  const bottomBarHeight =
+    visibleWindowHeight - document.documentElement.clientHeight;
 
-  const outerheight = window.outerHeight;
-
-  console.log("Height = ", height, ", Outer = ", outerheight);
-
+  // Update the content of the hero_texte div with bar heights
   const heroTexteDiv = document.querySelector(".test");
   if (heroTexteDiv) {
-    heroTexteDiv.innerHTML = `<p>Height: ${height}px, Outer:${outerheight}</p>`;
+    heroTexteDiv.innerHTML = `
+            <p>Top Bar Height: ${topBarHeight}px</p>
+            <p>Bottom Bar Height: ${bottomBarHeight}px</p>
+          `;
   }
 
-  const scheight = screen.availHeight;
+  //   const height =
+  //     window.innerHeight ||
+  //     document.documentElement.clientHeight ||
+  //     document.body.clientHeight;
 
-  console.log("Screen Height = ", scheight);
+  //   const outerheight = window.outerHeight;
 
-  const heroTexteDiv2 = document.querySelector(".test2");
-  if (heroTexteDiv2) {
-    heroTexteDiv2.innerHTML = `<p>Screen Height: ${scheight}px</p>`;
-  }
+  //   console.log("Height = ", height, ", Outer = ", outerheight);
+
+  //   const heroTexteDiv = document.querySelector(".test");
+  //   if (heroTexteDiv) {
+  //     heroTexteDiv.innerHTML = `<p>Height: ${height}px, Outer:${outerheight}</p>`;
+  //   }
+
+  //   const scheight = screen.availHeight;
+  //   const sheight = screen.height;
+
+  //   console.log("Avail Height = ", scheight);
+
+  //   const heroTexteDiv2 = document.querySelector(".test2");
+  //   if (heroTexteDiv2) {
+  //     heroTexteDiv2.innerHTML = `<p>Avail Height: ${scheight}px</p>`;
+  //   }
 });
